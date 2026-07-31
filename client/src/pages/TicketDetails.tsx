b06e0518379d8578b66c180d07c5f45ccbc676d6
Nycelinux <celina.ludwigs@fh-erfurt.de>
2026-07-31 03:43:14 +0200

    feat: extend ticket details with status change and delete actions

--- client/src/pages/TicketDetails.tsx
+++ client/src/pages/TicketDetails.tsx
@@ -1,10 +1,14 @@
-import { useParams, Link } from 'react-router-dom';
+import { useParams, Link, useNavigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
 import type { Ticket } from '../types/ticket';
-import { getTicketById, getTickets } from '../api/tickets';
+import { getTicketById } from '../api/tickets';
+import { useTicket } from '../hooks/useTickets';
+import '../styles/TicketDetails.scss';
 
 function TicketDetails() {
   const { id } = useParams();
+  const navigate = useNavigate();
+  const { deleteTicket, updateTicket, toggleTicketStatus } = useTicket();
   const [ticket, setTicket] = useState<Ticket | null>(null);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
@@ -33,16 +37,43 @@ function TicketDetails() {
   if (!ticket) {
     return <p>Ticket not found...</p>;
   }
+
+  async function handleDelete() {
+    if (!ticket) return;
+    const confirmed = window.confirm('are you sure you want to delete this ticket?');
+    if (!confirmed) return;
+    await deleteTicket(ticket.id);
+    navigate('/');
+  }
+
+  async function handleStatusChange() {
+    if (!ticket) return;
+    await toggleTicketStatus(ticket.id);
+    const updated = await getTicketById(ticket.id);
+    setTicket(updated);
+  }
+
   return (
     <div className="ticket-details">
-      <Link to="/">... Back to dashboard</Link>
-      <h1>{ticket.title}</h1>
-      <p>{ticket.description}</p>
-      <div>
-        Priority: <strong>{ticket.priority}</strong>
-      </div>
-      <div>
-        Status: <strong>{ticket.status}</strong>
+      <Link className="back-link" to="/">
+        ... Back to dashboard
+      </Link>
+      <div className="ticket-details-card">
+        <h1>{ticket.title}</h1>
+        <p>{ticket.description}</p>
+
+        <div className="ticket-info">
+          <div>
+            Priority: <strong>{ticket.priority}</strong>
+          </div>
+          <div>
+            Status: <strong>{ticket.status}</strong>
+          </div>
+        </div>
+        <div className="ticket-actions">
+          <button onClick={handleStatusChange}> Change Status</button>
+          <button onClick={handleDelete}> Delete ticket</button>
+        </div>
       </div>
     </div>
   );
